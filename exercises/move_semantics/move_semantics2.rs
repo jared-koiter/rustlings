866fac0750de2f7b367daf1ba405e5ac4817db54
Jared Koiter <jaredpkoiter@gmail.com>
2022-08-03 21:15:16 -0400

    Resolve move exercises

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,11 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    // easy way, make clone of vec0 : let mut vec1 = fill_vec(vec0.clone());
+    let mut vec1 = fill_vec_borrow(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -15,6 +14,19 @@ fn main() {
     vec1.push(88);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+
+    // mutable case
+    /*
+    let mut vec0 = Vec::new();
+
+    fill_vec_mutably(&mut vec0);
+
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec0.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec0.len(), vec0);
+    */
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
@@ -25,4 +37,20 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec.push(66);
 
     vec
+}
+
+fn fill_vec_borrow(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec_filled = vec.clone();
+
+    vec_filled.push(22);
+    vec_filled.push(44);
+    vec_filled.push(66);
+
+    vec_filled
+}
+
+fn fill_vec_mutably(vec: &mut Vec<i32>) {
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
 }
