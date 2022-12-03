231d0c087015f7097aa0d8b18542cdc7901f160e
Jared Koiter <jaredpkoiter@gmail.com>
2022-12-02 22:18:56 -0500

    Remove duplicate name from hashmap value

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -18,7 +18,6 @@ use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
 struct Team {
-    name: String,
     goals_scored: u8,
     goals_conceded: u8,
 }
@@ -33,22 +32,15 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
 
-        let team_1 = scores.entry(team_1_name.clone()).or_insert(Team {
-            name: team_1_name,
+        let team_1 = scores.entry(team_1_name).or_insert(Team {
             goals_scored: 0,
             goals_conceded: 0
         });
         team_1.goals_scored += team_1_score;
         team_1.goals_conceded += team_2_score;
 
-        let team_2 = scores.entry(team_2_name.clone()).or_insert(Team {
-            name: team_2_name,
+        let team_2 = scores.entry(team_2_name).or_insert(Team {
             goals_scored: 0,
             goals_conceded: 0
         });
